53f08e2b10001981c5af30cbe1f8aa81964311b7
Shakiran Sathiyanathan <shakiran.sathiyanathan@gmail.com>
2020-03-31 17:19:16 -0400

    Added post-signup page

---
+++ client/src/pages/post-signup/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import pSignupStyles from "./psignup.module.css";
+
+import React from 'react'
+
+export default function index() {
+  return (
+    <div>
+      Thanks for signing up! Login to get into you new account.
+    </div>
+  )
+}
