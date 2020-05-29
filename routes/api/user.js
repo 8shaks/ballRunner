7b9435fac62aa1c0c4f947686a0c9a3121b16a74
Shakiran Sathiyanathan <shakiran.sathiyanathan@gmail.com>
2020-05-29 00:54:00 -0400

    Refactored Code

--- routes/api/user.js
+++ routes/api/user.js
@@ -4,9 +4,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require("../../config/keys");
-const { check } = require('express-validator');
 const validateRegisterInput = require("../../validation/register");
-const validateLoginInput = require("../../validation/login");
 
 
 const User = require('../../models/User');
